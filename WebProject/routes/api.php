46a7b7030d1a3b91e8281db4465871e916d55a75
WING\Lenovo <thangthuan20001@gmail.com>
2024-11-30 10:41:16 +0700

    Thêm ctcls ctba controller và cập nhật api

--- WebProject/routes/api.php
+++ WebProject/routes/api.php
@@ -5,7 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BenhnhanController;
 use App\Http\Controllers\HsbaController;
 use App\Http\Controllers\BacsiController;
+use App\Http\Controllers\CtbaController;
 
+Route::post('/ctba', [CtbaController::class, 'store']);
+Route::put('/ctba/{maba}/{mabl}', [CtbaController::class, 'update']);
+Route::delete('/ctba/{maba}/{mabl}', [CtbaController::class, 'destroy']);
+Route::get('/benhly/{maba}', [BenhlyController::class, 'getTenbl']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -28,3 +33,8 @@ Route::post('/doctors', [BacsiController::class, 'store']);
 Route::get('/doctors/{id}', [BacsiController::class, 'show']);
 Route::put('/doctors/{id}', [BacsiController::class, 'update']);
 Route::delete('/doctors/{id}', [BacsiController::class, 'destroy']);
+
+Route::get('/ctcls/details/{maba}', [CtclsController::class, 'getCtclsDetails']);
+Route::post('/ctcls', [CtclsController::class, 'createCtcls']);
+Route::put('/ctcls/{maba}/{mapcn}', [CtclsController::class, 'updateCtcls']);
+Route::delete('/ctcls/{maba}/{mapcn}', [CtclsController::class, 'deleteCtcls']);
