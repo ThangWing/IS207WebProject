dbc3f65679c49d7e84d0459258e231ede7bd3d06
unknown <lequynhan1212004@gmail.com>
2024-12-16 20:36:08 +0700

    Em commnent may dong chua tao chu k xoa j

--- WebProject/routes/api.php
+++ WebProject/routes/api.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\CtbaController;
 Route::post('/ctba', [CtbaController::class, 'store']);
 Route::put('/ctba/{maba}/{mabl}', [CtbaController::class, 'update']);
 Route::delete('/ctba/{maba}/{mabl}', [CtbaController::class, 'destroy']);
-Route::get('/benhly/{maba}', [BenhlyController::class, 'getTenbl']);
+// Route::get('/benhly/{maba}', [BenhlyController::class, 'getTenbl']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -34,21 +34,21 @@ Route::get('/doctors/{id}', [BacsiController::class, 'show']);
 Route::put('/doctors/{id}', [BacsiController::class, 'update']);
 Route::delete('/doctors/{id}', [BacsiController::class, 'destroy']);
 
-Route::get('/ctcls/details/{maba}', [CtclsController::class, 'getCtclsDetails']);
-Route::post('/ctcls', [CtclsController::class, 'createCtcls']);
-Route::put('/ctcls/{maba}/{mapcn}', [CtclsController::class, 'updateCtcls']);
-Route::delete('/ctcls/{maba}/{mapcn}', [CtclsController::class, 'deleteCtcls']);
+// Route::get('/ctcls/details/{maba}', [CtclsController::class, 'getCtclsDetails']);
+// Route::post('/ctcls', [CtclsController::class, 'createCtcls']);
+// Route::put('/ctcls/{maba}/{mapcn}', [CtclsController::class, 'updateCtcls']);
+// Route::delete('/ctcls/{maba}/{mapcn}', [CtclsController::class, 'deleteCtcls']);
 
-Route::controller(CtnhapvienController::class)->group(function () {
-Route::post('/ctnhapvien', 'store'); // Thêm
-Route::delete('/ctnhapvien/{maba}/{mapb}', 'destroy'); // Xóa
-Route::put('/ctnhapvien/{maba}/{mapb}', 'update'); // Sửa
-Route::get('/ctnhapvien/details/{maba}', 'showDetails'); // Hiển thị chi tiết
-});
+// Route::controller(CtnhapvienController::class)->group(function () {
+// Route::post('/ctnhapvien', 'store'); // Thêm
+// Route::delete('/ctnhapvien/{maba}/{mapb}', 'destroy'); // Xóa
+// Route::put('/ctnhapvien/{maba}/{mapb}', 'update'); // Sửa
+// Route::get('/ctnhapvien/details/{maba}', 'showDetails'); // Hiển thị chi tiết
+// });
 
-Route::get('/', [CtkhambenhController::class, 'index']);
-Route::get('/{makb}', [CtkhambenhController::class, 'show']);
-Route::post('/create-and-count', [CtkhambenhController::class, 'createAndCount']);
-Route::put('/{makb}', [CtkhambenhController::class, 'update']);
-Route::delete('/{makb}', [CtkhambenhController::class, 'destroy']);
-Route::get('/mabn/{mabn}', [CtkhambenhController::class, 'getByMabn']);
+// Route::get('/', [CtkhambenhController::class, 'index']);
+// Route::get('/{makb}', [CtkhambenhController::class, 'show']);
+// Route::post('/create-and-count', [CtkhambenhController::class, 'createAndCount']);
+// Route::put('/{makb}', [CtkhambenhController::class, 'update']);
+// Route::delete('/{makb}', [CtkhambenhController::class, 'destroy']);
+// Route::get('/mabn/{mabn}', [CtkhambenhController::class, 'getByMabn']);
